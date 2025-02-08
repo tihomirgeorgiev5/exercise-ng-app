f04425b205d2b3263c3e960edbe094a502347d96
Tihomir Georgiev <georgiev.tm@abv.bg>
2025-02-08 12:24:51 +0100

    import new counter component in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { GreetingComponent } from '../components/greeting/greeting.component';
+import { CounterComponent } from '../components/counter/counter.component';
 
 @Component({
   selector: 'app-home',
-  imports: [GreetingComponent],
+  imports: [GreetingComponent, CounterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

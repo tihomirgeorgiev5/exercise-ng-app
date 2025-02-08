0c5468b94c670bae48657392aabd0fcc6dbe3fcf
Tihomir Georgiev <georgiev.tm@abv.bg>
2025-02-08 22:34:02 +0100

    added todos as a page inside in home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,4 +9,11 @@ export const routes: Routes = [
         },
 
     },
+    {
+        path: 'todos',
+        loadComponent: () => {
+            return import('./todos/todos.component').then((m) => m.TodosComponent)
+        },
+
+    }
 ];

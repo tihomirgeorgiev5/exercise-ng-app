202aa53fe28b434dcc8fcdadaf5558beacbaf91b
Tihomir Georgiev <georgiev.tm@abv.bg>
2025-02-09 12:41:54 +0100

    added successfully all data from api that we can see in our Todos on SPA

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -7,20 +7,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class TodosService {
   http = inject(HttpClient);
-todoItems: Array<Todo> = [
-  {
-    title: 'groceries',
-    id: 0,
-    userId: 1,
-    completed: false,
-  },
-  {
-    title: 'car wash',
-    id: 1,
-    userId: 1,
-    completed: false,
-  },
-];
+
 getTodosFromApi() {
   const url = `https://jsonplaceholder.typicode.com/todos`;
   return this.http.get<Array<Todo>>(url);

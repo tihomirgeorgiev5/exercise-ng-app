1303512586f6d2c8e5b53b3aa8a4801d5db3ac80
Tihomir Georgiev <georgiev.tm@abv.bg>
2025-02-09 11:35:35 +0100

    added implements OnInit in todos.components to can see objects

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { TodosService } from '../services/todos.service';
 
 @Component({
@@ -7,6 +7,10 @@ import { TodosService } from '../services/todos.service';
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit{
 todoService = inject(TodosService);
+
+ngOnInit(): void {
+  console.log(this.todoService.todoItems);
+}
 }

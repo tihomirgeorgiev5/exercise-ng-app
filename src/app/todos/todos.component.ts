08d345d60bf82d2d713092debfe77f059ab06e3d
Tihomir Georgiev <georgiev.tm@abv.bg>
2025-05-18 21:01:46 +0200

    added search folder using a Pipe

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -3,19 +3,21 @@ import { TodosService } from '../services/todos.service';
 import { Todo } from '../model/todo.type';
 import { catchError } from 'rxjs';
 import { TodoItemComponent } from '../components/todo-item/todo-item.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 
 @Component({
   selector: 'app-todos',
-  imports: [TodoItemComponent],
+  imports: [TodoItemComponent, FormsModule],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
 export class TodosComponent implements OnInit{
 todoService = inject(TodosService);
 todoItems = signal<Array<Todo>>([]);
+searchTerm = signal('');
 
 ngOnInit(): void {
   this.todoService

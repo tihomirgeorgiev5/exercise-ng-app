2d5d2362e20fa3ef0a1a5f46b33dcd3a406f2082
Tihomir Georgiev <georgiev.tm@abv.bg>
2025-05-18 11:44:58 +0200

    added appHighlightCompletedTodo

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -4,7 +4,7 @@ import { HighlightCompletedTodoDirective } from '../../directives/highlight-comp
 
 @Component({
   selector: 'app-todo-item',
-  imports: [],
+  imports: [HighlightCompletedTodoDirective],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.scss'
 })
